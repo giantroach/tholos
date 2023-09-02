d87d5b4ec70ae6cc37cdf9e61bad5ce6beea5ccf
Tomoki Motohashi <tomoki.motohashi@gmail.com>
2023-09-02 10:43:16 +0900

    fix asset dir path

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       output: {
         entryFileNames: `modules/app.js`,
         chunkFileNames: 'modules/chunk.js',
-        assetFileNames: `modules/[name].[ext]`,
+        assetFileNames: `img/[name].[ext]`,
       },
     },
   },
